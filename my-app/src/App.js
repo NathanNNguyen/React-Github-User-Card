5c6ede79e5e1b27fa3e6f80a8aa2c4c61367aa57
Nhan <natenguyen51294@gmail.com>
2020-01-14 15:53:52 -0600

    rendering followers info

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,55 +6,42 @@ class App extends React.Component {
 
   state = {
     infos: [],
+    followers: [],
   }
-  
+
 
   componentDidMount() {
     axios.get(`https://api.github.com/users/NathanNNguyen`)
       .then(res => {
-        this.setState({infos: res.data})
+        this.setState({ infos: res.data })
+        console.log(res.data)
+      })
+      .catch(err => console.log(err))
+
+    axios.get(`https://api.github.com/users/NathanNNguyen/followers`)
+      .then(res => {
+        this.setState({ followers: res.data })
         console.log(res.data)
       })
       .catch(err => console.log(err))
   }
 
-  componentDidUpdate() {
-    // axios.get(`https://api.github.com/users/NathanNNguyen/followers`)
-    //   .then(res => {
-    //     this.setState({infos: res.data})
-    //     console.log(res.data)
-    //   })
-    //   .catch(err => console.log(err))
-  }
-
-
-  // fetchFollowers = e => {
-  //   e.preventDefault();
-  //   axios
-  //     .get(`https://api.github.com/users/NathanNNguyen/followers`)
-  //     .then(res => {
-  //       this.setState({
-  //         infos: res.data.message
-  //       });
-  //       console.log(res);
-  //     })
-  //     .catch(err => console.log(err));
-  // };
-  
   render() {
     return (
       <div className="App">
-          <h1>My app</h1>
+        <h1>My Card</h1>
         <div>
           <p>{this.state.infos.name}</p>
           <p>{this.state.infos.location}</p>
           <p>{this.state.infos.bio}</p>
-          <img width="200" src={this.state.infos.avatar_url} alt='user-img'/>
+          <img width="200" src={this.state.infos.avatar_url} alt='user-img' />
         </div>
-          <button onClick={this.fetchFollowers}>Followers</button>
-          {/* {this.state.infos.map(info => (
-            <img width="200" src={this.state.infos.avatar_url} key={info} alt='follower-img' />
-          ))} */}
+
+        <div>
+          {this.state.followers.map(follower => (
+            <img width="150" src={follower.avatar_url} key={follower.id} />
+          ))}
+        </div>
       </div>
     );
   }
